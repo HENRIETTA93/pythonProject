27ab44c8da64353d1170b41c5fe4a67631fa2d00
henrietta <1355076874@qq.com>
2021-09-02 23:15:47 +0800

    psql

---
+++ assigns/Y2021_db/comp6240_q1/__init__.py
@@ -0,0 +1,20 @@
+# 1.1 How many drama movies (i.e., the major genre of the movie is drama) are produced in New Zealand? List that
+# number. (2 Marks)
+# 1.2 Which movies are classified as “Btl” in Sweden? List their titles and production years, and order them in the
+# descending order of their production years. (2 Marks)
+# 1.3 Which countries have at least three restriction categories? List these countries along with the corresponding
+# number of restriction categories. (2 Marks)
+# 1.4 Which movies have won at least one director or writer award? List their titles and production years. (2 Marks)
+# 1.5 For all movies produced in Germany, list their title, production year and the age of the director when the movie
+# was produced. (Hint: if a movie is produced in 1994 and its director was born in 1940, then the age of the
+# director when this movie was produced is 1994 - 1940 = 54) (2 Marks)
+# 1.6 What is the largest number of crew members working in a single movie? List that number. (2 Marks)
+# 1.7 Which directors have collaborated with at least two different writers? List the ids, first and last names of these
+# directors, and order them in the ascending order of their last names. (Hint: a director is considered to collaborate
+# with a writer if the director directed a movie written by the writer). (2 Marks)
+# 1.8 How many movies are written by a single writer? List that number. (Hint: a movie may be co-written by
+# multiple writers but this question asks for movies written by only one writer). (2 Marks)
+# 1.9 Among those directors who have never won any director award, who directed the largest number of movies? List
+# their ids. (2 Marks)
+# 1.10 Which directors have starred in every movie directed by themselves? List their ids, first names and last names.
+# (Hint: a person is considered to star in a movie if this person plays at least one role in this movie) (2 Marks)
---
+++ assigns/Y2021_db/comp6240_q1/ans.sql
@@ -0,0 +1,91 @@
+#1
+select count(*) from MOVIE where country='New Zealand' and major_genre='drama';
+
+#2
+select m.title, m.production_year from MOVIE m join RESTRICTION r
+on m.title=r.title and m.production_year=r.production_year
+where r.country='Sweden' and r.description='Btl'
+order by m.production_year desc;
+
+#3
+select country, count(description)
+from RESTRICTION_CATEGORY
+group by country
+having count(description)>=3;
+
+#4
+select m.title, m.production_year
+from MOVIE m join DIRECTOR_AWARD ma on m.title=ma.title and m.production_year=ma.production_year
+union
+select m.title, m.production_year
+from MOVIE m join WRITER_AWARD ma on m.title=ma.title and m.production_year=ma.production_year;
+
+#5
+select m.title, m.production_year, m.production_year-p.year_born as age
+from MOVIE m join DIRECTOR d on m.title=d.title and m.production_year=d.production_year
+join PERSON p on d.id=p.id
+where m.country='Germany';
+
+#6
+select number_crews
+from
+(
+select c.title, c.production_year, count(c.id) as number_crews
+from CREW
+group by c.title, c.production_year
+order by count(c.id) desc
+) t;
+
+
+#7
+select p.id,p.first_name, p.last_name
+from
+(
+select d.id, count(distinct w.id)
+from DIRECTOR d join MOVIE m on d.title=m.title and d.production_year=m.production_year
+join WRITER w on w.title=m.title and w.production_year=m.production_year
+group by d.id
+having count(distinct w.id)>=2
+) t join PERSON p on t.id=p.id
+order by p.last_name;
+
+#8
+select count(*)
+from
+(
+select w.title, w.production_year, count(w.id)
+from WRITER w
+group by w.title, w.production_year
+having count(w.id)=1
+) t;
+
+
+#9
+select t.id
+(
+select distinct d.id
+from DIRECTOR d
+left join DIRECTOR_AWARD da
+on d.title = da.title and d.production_year = da.production_year
+where da.award_name is null
+) t join DIRECTOR d1 on t.id=d1.id
+group by t.id
+order by count(d1.title) desc limit 1;
+
+#10
+select t1.id, p.first_name, p.last_name
+from
+(
+select d.id, count(d.title) as m_num
+from DIRECTOR d join ROLE r
+on d.title=r.title and d.production_year=r.production_year
+group by d.id
+) t1
+join
+(
+select d.id, count(d.title) as m_num
+from DIRECTOR d
+group by d.id
+) t2 on t1.m_num=t2.m_num
+join
+PERSON p on t1.id=p.id;
