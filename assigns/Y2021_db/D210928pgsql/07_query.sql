685b3d32a974c7e08189b69f685edd84aeaa2d2a
Henrietta <1355076874@qq.com>
2021-10-01 23:48:18 +0800

    mysql

--- assigns/Y2021_db/D210928pgsql/07_query.sql
+++ assigns/Y2021_db/D210928pgsql/07_query.sql
@@ -2,11 +2,11 @@
 -- 7
 -------------------------------
 
-select c.category_name,s.country as supplier_region, sum(unit_in_stock) as units_in_stock,
+select c.category_name,cr.region as supplier_region, sum(unit_in_stock) as units_in_stock,
 sum(unit_on_order) as units_on_order, reorder_level
 from categories c join products p on c.category_id=p.category_id
 join suppliers s on s.supplier_id=p.supplier_id
 join country_region cr on s.country=cr.country
 where cr.region in ('America','Europe','Asia-Pacific')
-group by c.category_name, s.country, reorder_level
+group by c.category_name, cr.region, reorder_level
 order by c.category_name, supplier_region, reorder_level;
