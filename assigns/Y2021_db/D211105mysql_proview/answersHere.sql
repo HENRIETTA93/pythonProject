60379856743f322b03921d207e872466de407e78
henrietta <1355076874@qq.com>
2021-11-05 16:52:34 +0800

    mysql

---
+++ assigns/Y2021_db/D211105mysql_proview/answersHere.sql
@@ -0,0 +1,35 @@
+##################################################
+### Put your queries in this file and resubmit ###
+##################################################
+
+### PART 1 Cleaning Data ###
+
+# 1.) Run messydata.sql. Check if there are any duplicate columns with the same name. 
+# Remove any duplicate columns using Alter Table.
+
+# 2.) Convert the single table into Relational Tables.
+
+# 3.) Add unique primary keys in each table.
+
+# 4.) Add foreign keys to each table.
+
+# 5.) Reverse engineer a schema and create relations in the editor. Export and upload
+
+### Part 2 Procedures, Views and Functions ###
+
+# 1.) Create a procedure that, given a three didgit country code, returns the percent 
+# change in GDPin the string format "3.14%". If there is a NULL value return "Not Enough Data".
+# Call the function with an example.
+
+# 2.) Create a function that uses the above procedure. Select a table with the associated percent 
+# change in GDP as a new column for each country code
+
+# 3.) Save the table from Question 2 as a view.
+
+# 4.) Expanding on the view from 3, add another column that is the average population 
+# of the cities within a country.
+
+# 5.) Full join all of your tables and return cities that that start with the first 
+# letter of your first name and end with the last letter of your last name.
+
+
