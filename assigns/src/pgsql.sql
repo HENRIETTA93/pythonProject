abe0a193b6be7b87df09a8c2ba6ea98cbed5549d
henrietta <1355076874@qq.com>
2021-11-24 18:01:25 +0800

    pgsql.sql

--- assigns/src/pgsql.sql
+++ assigns/src/pgsql.sql
@@ -1,4 +1,4 @@
-
+\COPY tablename FROM filename CSV [HEADER] [NULL '…']
 -- 常用日期函数
 
 --# 获取年、月、日
@@ -13,3 +13,13 @@ select extract(month from now());
 select date_part('day', now());
 
 --# 计算日期差
+
+
+
+###### trigger #####
+CREATE TRIGGER TriggerName
+{AFTER|BEFORE}  Event1 [OR Event2 ...]
+ON TableName
+[ WHEN ( Condition ) ]
+FOR EACH {ROW|STATEMENT}
+EXECUTE PROCEDURE FunctionName(args...);
