a596c3dada4865811af559ada0dc1a4dea215dc6
henrietta <1355076874@qq.com>
2022-03-17 16:36:57 +0800

    220

---
+++ assigns/Y2022_dbexa/ucl220/ExampleTests.java
@@ -0,0 +1,206 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+import org.junit.Ignore;
+
+
+public class ExampleTests {
+
+
+	private int multiply(int a,int b) {
+		return(a*b);
+	}
+
+
+@Test
+public void test1() {
+}
+@Test
+public void test2() {
+}
+@Test
+public void test31() {
+}
+@Test
+public void test3() {
+}
+@Test
+public void test4() {
+}
+@Test
+public void test5() {
+}
+ @Test
+public void test6() {
+}
+ @Test
+public void test7() {
+}
+ @Test
+public void test8() {
+}
+ @Test
+public void test9() {
+}
+ @Test
+public void test10() {
+}
+ @Test
+public void test11() {
+}
+ @Test
+public void test12() {
+}
+
+
+
+
+@Test
+public void testa1() {
+}
+@Test
+public void testa2() {
+}
+@Test
+public void testa31() {
+}
+@Test
+public void testa3() {
+}
+@Test
+public void testa4() {
+}
+@Test
+public void testa5() {
+}
+@Test
+public void testa6() {
+}
+ @Test
+public void testa7() {
+}
+ @Test
+public void testa8() {
+}
+ @Test
+public void testa9() {
+}
+ @Test
+public void testa10() {
+}
+ @Test
+public void testa11() {
+}
+ @Test
+public void testa12() {
+}
+
+
+
+@Test
+public void teste1() {
+}
+@Test
+public void teste2() {
+}
+@Test
+public void teste31() {
+}
+@Test
+public void teste3() {
+}
+@Test
+public void teste4() {
+}
+@Test
+public void teste5() {
+}
+ @Test
+public void teste6() {
+}
+ @Test
+public void teste7() {
+}
+ @Test
+public void teste8() {
+}
+ @Test
+public void teste9() {
+}
+ @Test
+public void teste10() {
+}
+ @Ignore @Test
+public void teste11() {
+}
+ @Test
+public void teste12() {
+}
+
+
+
+@Test
+public void testf1() {
+}
+@Test
+public void testf2() {
+}
+@Test
+public void testf31() {
+}
+@Test
+public void testf3() {
+}
+@Test
+public void testf4() {
+}
+@Test
+public void testf5() {
+}
+ @Test
+public void testf6() {
+}
+
+@Ignore @Test
+public void testf7() {
+}
+ @Test
+public void testf8() {
+}
+ @Test
+public void testf9() {
+}
+ @Test
+public void testf10() {
+}
+ @Test
+public void testf11() {
+}
+ @Test
+public void testf12() {
+	Random rnd=new Random();
+	if (Math.abs(rnd.nextInt() % 100)<20) {
+		assertEquals("OK","NOTOK");
+	} else {
+		assertEquals("OK","OK");
+	}
+}
+
+
+
+
+
+
+
+	public static void main(String args[]) {
+
+
+		System.out.println("Running main for Person");
+
+	}
+
+
+
+} 
