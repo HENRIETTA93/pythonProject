ebccf316e78f5899c3a186a950d00e5d1700febc
lucid <henrietta@qq.com>
2022-06-22 01:32:50 +0800

    zh

--- assigns/Y2022_db/zh_sql/工资信息管理系统.sql
+++ assigns/Y2022_db/zh_sql/工资信息管理系统.sql
@@ -1,24 +1,46 @@
-create table dep (
+create database salary_system
+ON
+( NAME =salary_system_data,
+	  FILENAME = 'D:\salary_system_data.mdf',
+	  SIZE=5MB,
+      MAXSIZE = 20MB,
+      FILEGROWTH = 10%)
+LOG ON
+( NAME = SCT电气学院068_log,
+      FILENAME ='D:\salary_system_log.ldf',
+      SIZE=3MB,
+      MAXSIZE = 15MB,
+      FILEGROWTH = 10%)
+GO
+
+use salary_system
+
+go
+create table dept (
 	id int not null  primary key ,
 	name varchar(20) not null ,
-	manager_id varchar(10) not null ,
-	foreign key (emp_id) references emp(id)
+	manager_id varchar(10)
 );
 
-insert into dep values(1,'信息部','emp1001');
-insert into dep values(2,'销售部','emp1002');
-insert into dep values(3,'财务部','emp1003');
 
 create table emp(
 	id varchar(10) primary key not null ,
 	name varchar(20) not null ,
 	sex varchar(5) check( sex in ( '男', '女')) ,
 	birth date ,
-	tel varchar(20) ,
+	telphone varchar(20) ,
 	education varchar(20) ,
-	dep_id int not null ,
- 	foreign key (dep_id) references dep(id)
+	dept_id int not null ,
+ 	foreign key (dept_id) references dept(id)
 );
+
+alter table dept add constraint fk_manager foreign key(manager_id) references emp(id);
+
+insert into dept values(1,'信息部',null);
+insert into dept values(2,'销售部',null);
+insert into dept values(3,'财务部',null);
+
+
 
 insert into emp values('emp1001' , '李晓波' ,'男' , '1998-01-01' , '13900000000' , '本科' , '1') ,
 ('emp1002' , '刘慈欣' ,'女' , '1998-03-02' , '13526457801' , '硕士' , '2') ,
@@ -30,30 +52,34 @@ insert into emp values('emp1001' , '李晓波' ,'男' , '1998-01-01' , '13900000
 ('emp2005' , '李五五' ,'男' , '1999-06-02' , '13530157801' , '硕士' , '2') ,
 ('emp2006' , '张菲菲' ,'女' , '1999-06-03' , '13530167801' , '本科' , '3') ;
 
+
+update dept set manager_id='emp1001' where id=1;
+update dept set manager_id='emp1002' where id=2;
+update dept set manager_id='emp1003' where id=3;
+
 create table salary(
 	id varchar(20) primary key not null ,
 	emp_id varchar(10) not null ,
-	jiben money ,
-	jiaban money ,
-	jiangjin money ,
-	kouchu money ,
-	paytime date ,
-	foreign key (id) references emp(id)
+	base money ,
+	extra money ,
+	reward money ,
+	deduct money ,
+	paydate date ,
+	foreign key (emp_id) references emp(id)
 );
 
-insert into salary values('SA10068emp1001','emp1001',8000,500,1000,0,'2020-07-28'),
-('SA10068emp2001','emp2001',6000,500,800,0,'2020-07-30'),
-('SA10068emp2004','emp2004',6000,500,800,100,'2020-07-30'),
+insert into salary values('salary01_emp1001','emp1001',8000,500,1000,0,'2022-06-30'),
+('salary01_emp2001','emp2001',6000,500,800,0,'2022-05-30'),
+('salary02_emp2001','emp2001',6000,500,800,0,'2022-04-30'),
+('salary01_emp2004','emp2004',6000,500,800,100,'2022-02-28'),
+('salary01_emp1002','emp1002',8000,100,1000,0,'2022-04-28'),
+('salary01_emp2002','emp2002',6500,0,600,50,'2022-05-30'),
+('salary01_emp2005','emp2005',6500,200,100,0,'2022-06-30'),
+('salary01_emp1003','emp1003',8000,0,1000,0,'2022-05-28'),
+('salary01_emp2003','emp2003',7000,300,200,0,'2022-06-30'),
+('salary01_emp2006','emp2006',6000,200,200,0,'2022-04-30');
 
-('SA100681001004','emp1002',8000,100,1000,0,'2020-07-28'),
-('SA100681001005','emp2002',6500,0,600,50,'2020-07-30'),
-('SA100681001006','emp2005',6500,200,100,0,'2020-07-30'),
-
-('SA100681001007','emp1003',8000,0,1000,0,'2020-07-28'),
-('SA100681001008','emp2003',7000,300,200,0,'2020-07-30'),
-('SA100681001009','emp2006',6000,200,200,0,'2020-07-30')；
-
-create table checkon(
+create table worktime(
 	id varchar(20) primary key not null ,
 	emp_id varchar(10) not null ,
 	check_in_time datetime ,
@@ -62,17 +88,17 @@ create table checkon(
 
 );
 
-insert into checkon values ( 'CK100860001' , 'emp1001' , '2020-07-15 09:00:00' , '2020-07-15 17:20:00' ) ,
- ( 'CK100860002' , 'emp1002' , '2020-07-15 09:00:00' , '2020-07-15 17:15:00' ) ,
- ( 'CK100860003' , 'emp1003' , '2020-07-15 09:00:00' , '2020-07-15 18:10:00' ) ,
- ( 'CK100860004' , 'emp2001' , '2020-07-15 09:00:00' , '2020-07-15 17:02:00' ) ,
- ( 'CK100860005' , 'emp2002' , '2020-07-15 09:00:00' , '2020-07-15 18:03:00' ) ,
- ( 'CK100860006' , 'emp2003' , '2020-07-15 09:10:00' , '2020-07-15 17:04:00' ) ,
- ( 'CK100860007' , 'emp2004' , '2020-07-15 09:00:00' , '2020-07-15 17:05:00' ) ,
- ( 'CK100860008' , 'emp2005' , '2020-07-15 09:00:00' , '2020-07-15 17:06:00' ) ,
- ( 'CK100860009' , 'emp2006' , '2020-07-15 09:20:00' , '2020-07-15 19:17:00' ) ;
+insert into worktime values ( 'work0001' , 'emp1001' , '2022-06-15 09:00:00' , '2022-06-15 17:20:00' ) ,
+ ( 'work0002' , 'emp1002' , '2022-06-15 09:00:00' , '2022-06-15 17:15:00' ) ,
+ ( 'work0003' , 'emp1003' , '2022-06-15 09:00:00' , '2022-06-15 18:10:00' ) ,
+ ( 'work0004' , 'emp2001' , '2022-06-15 09:00:00' , '2022-06-15 17:02:00' ) ,
+ ( 'work0005' , 'emp2002' , '2022-06-15 09:00:00' , '2022-06-15 18:03:00' ) ,
+ ( 'work0006' , 'emp2003' , '2022-06-15 09:10:00' , '2022-06-15 17:04:00' ) ,
+ ( 'work0007' , 'emp2004' , '2022-06-15 09:00:00' , '2022-06-15 17:15:00' ) ,
+ ( 'work0008' , 'emp2005' , '2022-06-15 09:00:00' , '2022-06-15 14:06:00' ) ,
+ ( 'work0009' , 'emp2006' , '2022-06-15 09:20:00' , '2022-06-15 19:17:00' ) ;
 
-create table leave(
+create table onleave(
 	id varchar(20) primary key not null ,
 	emp_id varchar(10) not null ,
 	leave_start_time datetime ,
@@ -80,16 +106,18 @@ create table leave(
 	foreign key (emp_id) references emp(id)
 );
 
-insert into leave values ('LE1001230001' , 'emp2004' , '2020-07-18 09:00:00' , '2020-07-20 14:00:00'),
-('LE1001230001' , 'emp2002' , '2020-07-20 14:00:00' , '2020-07-21 14:00:00');
+insert into onleave values ('1' , 'emp2004' , '2022-06-18 09:00:00' , '2022-06-21 14:00:00'),
+('2' , 'emp2002' , '2022-06-12 14:00:00' , '2022-06-13 14:00:00');
 
 
-====视图======
+go
+-- 视图
+go
 create view V_dep_manage_info
 as
-select d.id as dep_id , d.name as dep_name , e.name as emp_name , e.sex as emp_sex , e.birth as emp_birth , e.tel as emp_tel , e.edu as emp_edu
-from dep d , emp e
-where d.emp_id = e.id;
+select d.id as dept_id , d.name as dep_name , e.name as emp_name , e.sex as emp_sex , e.birth, e.telphone , e.education
+from dept d , emp e
+where d.manager_id = e.id;
 
 go
 
@@ -97,26 +125,26 @@ select * from V_dep_manage_info
 
 
 
+go
 
-
-create view V_emp_actualsalary
+create view V_emp_real_salary
 as
-select e.id , e.name , (s.jiben + s.jiaban + s.jiangjin - s.kouchu ) as actual_salary
+select e.id , e.name , (s.base + s.extra + s.reward - s.deduct) as real_salary
 from emp e , salary s
 where e.id = s.emp_id;
 
 go
 
-select * from V_emp_actualsalary;
+select * from V_emp_real_salary;
 
 
 
 
-
+go
 create view V_emp_kaoqin
 as
-select e.id , e.name , c.check_in_time , c.check_out_time , (c.check_out_time - c.check_in_time ) as 在勤时间
-from emp e , checkon c
+select e.id , e.name , c.check_in_time , c.check_out_time , (c.check_out_time - c.check_in_time ) as work_time
+from emp e , worktime c
 where e.id = c.emp_id;
 
 go
@@ -124,11 +152,11 @@ go
 select * from V_emp_kaoqin;
 
 
-
+go
 create view V_emp_leavenum
 as
 select e.id , min(e.name) as name , count(l.id) as leave_num
-from emp e , leave l
+from emp e , onleave l
 where e.id = l.emp_id
 group by e.id
 
@@ -140,10 +168,10 @@ select * from V_emp_leavenum;
 
 
 
-
+go
 create view V_emp_salaryhis
 as
-select e.id , e.name , s.jiben , s.jiaban , s.kouchu , s.paytime
+select e.id , e.name , s.base + s.extra + s.reward-s.deduct as salary , s.paydate
 from emp e , salary s
 where e.id = s.emp_id;
 
@@ -153,54 +181,57 @@ select * from V_emp_salaryhis;
 
 
 
-===查询、更新===
+-- 查询、更新
 /* 查询 员工及所属部门 */
-select * from emp e , dep d
-where e.dep_id = d.id;
+go
+select * from emp e , dept d
+where e.dept_id = d.id;
 
-/* 查询员工7月份工资 */
-select * ,(s.jiben + s.jiangjin + s.jiaban - s.kouchu) as 实发工资 from emp e , salary s
-where e.id = s.emp_id and s.paytime like '%-07-%';
+/* 查询员工2022年6月份工资 */
+go
+select * ,(s.base + s.reward+ s.extra - s.deduct) as 实发工资 from emp e , salary s
+where e.id = s.emp_id and s.paydate like '2022-06-%';
 
-/* 查询各部门7月份平均工资 */
-select e.dep_id , avg(s.jiben + s.jiangjin + s.jiaban - s.kouchu) from emp e , salary s
-where e.id = s.emp_id and s.paytime like '%-07-%'
-group by e.dep_id;
+/* 查询各部门2022年6月份平均工资 */
+go
+select e.dept_id , avg(s.base + s.reward + s.extra - s.deduct) as avg_salary from emp e , salary s
+where e.id = s.emp_id and s.paydate like '2022-06-%'
+group by e.dept_id;
 
-/* 查询编号为 emp2004 的员工的请假记录 */
+/* 查询编号为 emp2002 的员工的请假记录 */
+go
+select * from emp e , onleave l
+where e.id = l.emp_id and e.id = 'emp2002';
 
-select * from emp e , leave l
-where e.id = l.emp_id and e.id = 'emp2004';
 
-
-/* 查询各个员工2020-07-15号的考勤记录，按签出时间排序 */
-
-select * from emp e , checkon c
-where e.id = c.emp_id and convert(varchar , c.check_in_time , 23) = '2020-07-15'
+/* 查询各个员工2022-06-15号的考勤记录，按签出时间排序 */
+go
+select * from emp e , worktime c
+where e.id = c.emp_id and convert(varchar , c.check_in_time , 23) = '2022-06-15'
 order by c.check_out_time;
 
 
 
 /* 更新 将所有部门的普通员工的基本工资改为6500 */
+go
+select emp_id , base from salary where emp_id in ( select e.id  from emp e , dept d where e.dept_id = d.id and e.id <> d.manager_id);
+go
+update salary set base=6500 where emp_id in (
 
-select emp_id , jiben from salary where emp_id in ( select e.id  from emp e , dep d where e.dep_id = d.id and e.id <> d.emp_id);
-
-update salary set jiben=6500 where emp_id in (
-
-	select e.id from  emp e , dep d
-	where e.dep_id = d.id and e.id <> d.emp_id
+	select e.id from  emp e , dept d
+	where e.dept_id = d.id and e.id <> d.manager_id
 	)
-
-select emp_id , jiben from salary where emp_id in ( select e.id  from emp e , dep d where e.dep_id = d.id and e.id <> d.emp_id);
-
-
+go
+select emp_id , base from salary where emp_id in ( select e.id  from emp e , dept d where e.dept_id = d.id and e.id <> d.manager_id);
 
 
-===触发器===
+
+
+-- 触发器
 /* 当插入新数据到考勤表时，如果新数据的签出时间晚于19:00:00，在薪水表中给该员工的加班工资添加100元 */
-
-create trigger T_inserttocheckon
-on checkon
+go
+create trigger trg_inserttoworktime
+on worktime
 instead of insert
 as
 declare @checkouttime datetime
@@ -208,7 +239,7 @@ declare @empid varchar(10)
 select @checkouttime = check_out_time , @empid = emp_id from inserted
 if(convert(varchar , @checkouttime , 8) > '19:00:00')
 begin
-update salary set jiaban = jiaban+10  where emp_id = @empid
+update salary set extra = extra+10  where emp_id = @empid
 select * from salary where emp_id = @empid
 end
 
@@ -218,9 +249,9 @@ go
 
 /* 当插入新数据到请假表时，如果该员工当月请假次数已大于等于3次，则给该员工的当月工资扣除50元 */
 
-
-create trigger T_inserttoleave
-on leave
+go
+create trigger trg_inserttoleave
+on onleave
 instead of insert
 as
 declare @num int
@@ -230,97 +261,68 @@ select @empid = emp_id , @leavestarttime = leave_start_time from inserted;
 select @num = count(emp_id) from leave where emp_id = @empid and datepart(m,leave_start_time) = datepart(m,@leavestarttime)
 if(@num >=3)
 begin
-update salary set kouchu = kouchu + 50 where emp_id = @empid and datepart(m,paytime) = datepart(m,@leavestarttime)
+update salary set deduct = deduct + 50 where emp_id = @empid and datepart(m,paydate) = datepart(m,@leavestarttime)
 end
 
 
 
-===存储过程===
+-- 存储过程
 
 
-/* 参数a，给各部门领导添加a元奖金 */
-
-create procedure P_addjiangjin
-@jj money
+/* 参数add_num，给各部门领导添加add_num元奖金 */
+go
+create procedure pro_addreward
+@add_num money
 as
 begin
-with a as ( select emp_id  from dep group by emp_id )
-update salary set jiangjin = jiangjin + @jj where emp_id in (select * from a)
+
+update salary set reward = reward + @add_num where emp_id in (select * from ( select manager_id from dept group by manager_id) t)
 end
 
 go
 
-select emp_id , jiangjin from salary where emp_id in ( select emp_id from dep )
-exec P_addjiangjin 200
-select emp_id , jiangjin from salary where emp_id in ( select emp_id from dep )
+select emp_id , reward from salary where emp_id in ( select manager_id from dept )
+exec pro_addreward 200
+select emp_id , reward from salary where emp_id in ( select manager_id from dept )
 
 
 
 /* 参数a，查询a部门的工资排序 */
-
-create procedure P_querydepsal
+go
+create procedure pro_querydepsal
 @depid varchar(20)
 as
 begin
-select e.id , e.name , s.jiben , s.jiangjin , s.jiaban , s.kouchu , (s.jiben + s.jiangjin + s.jiaban - s.kouchu) as actual from dep d , emp e , salary s
-where e.id = s.emp_id and e.dep_id  = d.id and d.id = @depid
-order by (s.jiben+s.jiangjin+s.jiaban-s.kouchu)
+select e.id , e.name , s.base , s.reward , s.extra , s.deduct , (s.base + s.reward + s.extra - s.deduct) as actual from dept d , emp e , salary s
+where e.id = s.emp_id and e.dept_id  = d.id and d.id = @depid
+order by (s.base+s.reward+s.extra-s.deduct)
 end
 
 go
 
-exec P_querydepsal 1
-exec P_querydepsal 2
-exec P_querydepsal 3
+exec pro_querydepsal 1
+exec pro_querydepsal 2
+exec pro_querydepsal 3
 
 
 
 /* 查询实发工资在参数a和参数b之间的员工信息 */
-
-create procedure P_querysalary
+go
+create procedure pro_querysalary
 @a money ,
 @b money
 as
 begin
-	select * from emp e , V_emp_actualsalary ac where e.id = ac.id  and ac.actual_salary between @a and @b ;
+	select * from emp e , V_emp_real_salary ac where e.id = ac.id  and ac.real_salary between @a and @b ;
 
 end
 
 go
 
-exec P_querysalary 6900 , 8000
+exec pro_querysalary 6900 , 8000
 go
 
 
-===用户及权限===
-/* 创建普通员工用户 */
-Create login GZ2020emp with PASSWORD=’123456’
-GO
-Create USER GZemp FOR LOGIN GZ2020emp
-Go
-/* 授权 */
-GRANT SELECT ON checkon to GZemp
-GRANT SELECT ON leave to GZemp
-GRANT SELECT ON salary to GZemp
-GO
-
-/* 创建主管用户 */
-Create login GZ2020man with PASSWORD=’123456’
-GO
-Create user GZman FOR LOGIN GZ2020man
-GO
-
-/* 授权 */
-GRANT SELECT ON checkon to GZman
-GRANT SELECT ON leave to GZman
-GRANT SELECT ON salary to GZman
-GRANT SELECT ON dep to GZman
-GRANT SELECT ON emp to GZman
-GO
-
-
-/* 收回权限 */
-REVOKE SELECT ON leave from GZemp
 
 
 
--- assigns/Y2022_db/zh_sql/药品存销信息管理系统.sql
+++ assigns/Y2022_db/zh_sql/药品存销信息管理系统.sql
@@ -25,7 +25,16 @@ create table 员工(
 学历 varchar(20),
 职务 varchar(20)
 );
-
+go
+alter table 员工 add constraint chk_sex check(性别 in ('男','女'));
+go
+insert into 员工 values (1,'李冰冰','女',45,'本科','销售');
+insert into 员工 values (2,'大张伟','男',40,'本科','仓库管理');
+insert into 员工 values (3,'杨幂','女',31,'本科','仓库管理');
+insert into 员工 values (4,'王思聪','男',35,'硕士','售前咨询');
+insert into 员工 values (5,'张靓颖','女',35,'硕士','销售');
+insert into 员工 values (6,'周杰伦','男',35,'硕士','销售');
+go
 create table 药品(
 药品编号 int primary key,
 药品名称 varchar(20) not null,
@@ -38,124 +47,202 @@ create table 药品(
 经手人 int
 );
 
-
+alter table 药品 add constraint fk_jingshou foreign key (经手人) references 员工(工号);
+go
+insert into 药品 values (101,'六味地黄丸','江苏制药', getdate(),180,'解毒',20,20, 2);
+insert into 药品 values (102,'黄芪','山东中药制药','2022-04-29',365,'降火',32,100,1);
+insert into 药品 values (103,'三黄片','江苏中药局','2022-04-29',360,'降火',3 ,100,3);
+insert into 药品 values (104,'解毒丸','浙江草药局','2022-04-29',360,'解毒',15,100,1);
+insert into 药品 values (105,'止痛丸','安徽中药局','2022-04-29',360,'止痛',20,100,4);
+insert into 药品 values (106,'金银花','河南中药局','2022-04-29',360,'解暑',12,100,1);
+go
 create table 客户(
 客户编号 int primary key,
 姓名 varchar(20) not null,
 联系方式 varchar(11)
 );
-
+go
+insert into 客户 values(201, '张晓北','132457852');
+insert into 客户 values(202, '张一北','132457252');
+insert into 客户 values(203, '张二北','133457353');
+insert into 客户 values(204, '张三北','134457454');
+insert into 客户 values(205, '张四北','135457555');
+insert into 客户 values(206, '张五北','136457656');
+go
 create table 购买(
 购买编号 int primary key,
 客户编号 int,
 药品编号 int,
 购买时间 date,
+销售员 int,
 数量 int
 );
+go
+alter table 购买 add constraint fk_kehu foreign key (客户编号) references 客户(客户编号);
+alter table 购买 add constraint fk_yaopin foreign key (药品编号) references 药品(药品编号);
+alter table 购买 add constraint fk_xiaoshou foreign key (销售员) references 员工(工号);
+go
+insert into 购买 values(1,201, 104,getdate(),1, 2);
+insert into 购买 values(2,201, 101,getdate(),2, 1);
+insert into 购买 values(3,201, 102,getdate(), 1,1);
+insert into 购买 values(4,202, 103,'2022-06-19', 2,2);
+insert into 购买 values(5,203, 104,'2022-06-19', 3,5);
+insert into 购买 values(6,204, 105,'2022-06-19', 4,5);
+insert into 购买 values(7,205, 106,'2022-06-19', 4,5);
+insert into 购买 values(8,206, 102,'2022-06-19', 2,4);
+insert into 购买 values(9,202, 104,'2022-06-19', 3,4);
+insert into 购买 values(10,201, 103,getdate(), 3,2);
 
+go
 create table 入库信息(
 入库编号 int primary key,
 药品编号 int not null,
-进货日期 date not null default (getdate()),
-生产日期 date,
-进货单价 decimal not null,
-进货数量 int not null,
-constraint fk_yaopin foreign key (药品编号) references 药品(药品编号)
+入库日期 date not null default (getdate()),
+入库位置 varchar(20),
+入库数量 int not null,
+constraint fk_yaopinrk foreign key (药品编号) references 药品(药品编号)
 );
 
-alter table 进货信息 add constraint chk_danjia check(进货单价>0);
-alter table 进货信息 add constraint chk_shuliang check(进货数量>0);
-alter table 进货信息 add constraint chk_youxiao check(进货日期<有效期);
-
-create table 销售员(
-工号 int primary key,
-姓名 varchar(20) not null,
-性别 varchar(2) not null,
-住址 varchar(30)
-);
+go
+insert into 入库信息 values (101001,101,'2022-06-07','位置1',20);
+insert into 入库信息 values (101002,101,'2022-06-07','位置1',10);
+insert into 入库信息 values (102001,102,'2022-06-07','位置2',15);
+insert into 入库信息 values (103001,103,'2022-06-17','位置3',26);
+insert into 入库信息 values (104001,104,'2022-06-17','位置4',28);
+insert into 入库信息 values (105001,105,'2022-06-17','位置5',20);
+insert into 入库信息 values (106001,106,'2022-06-17','位置6',200);
 
 
-create table 销售信息(
-进货编号 int not null,
-工号 int not null,
-销售单价 decimal,
-柜台号 varchar(20) not null,
+go
+create table 出库信息(
+出库编号 int not null,
+药品编号 int not null,
 数量 int not null,
-销售日期 datetime not null default (getdate()),
-primary key(进货编号,工号,销售日期),
-constraint fk_gonghao foreign key (工号) references 销售员(工号),
-constraint fk_yaop foreign key (进货编号) references 进货信息(进货编号)
+出库日期 date not null default (getdate()),
+primary key(出库编号),
+constraint fk_yaopinck foreign key (药品编号) references 药品(药品编号)
 );
 
 
-create table 库存(
-进货编号 int primary key,
-库存数量 int,
-constraint fk_yp foreign key (进货编号) references 进货信息(进货编号)
-);
+
+go
+insert into 出库信息 values (101001,101,30,getdate());
+insert into 出库信息 values (101002,102,35,getdate());
+insert into 出库信息 values (101003,103,32,getdate());
+insert into 出库信息 values (102001,101,15,getdate());
+insert into 出库信息 values (103001,104,20,getdate());
+
+-- 创建触发器，当药品入库时，同时更新药品的数量。
+go
+create trigger trig_update_yaopin
+on 入库信息
+after insert
+as
+begin
+declare @yaopin int
+declare @num int
+select @yaopin=药品编号,@num=入库数量 from inserted
+update 药品 set 数量=数量+@num where 药品编号=@yaopin
+
+end
 
 
-insert into 药品 values (101,'中药','六味地黄丸','江苏');
-insert into 药品 values (102,'草药','黄芪','山东');
-insert into 药品 values (103,'中成药','三黄片','江苏');
-insert into 药品 values (104,'中药','解毒丸','浙江');
-insert into 药品 values (105,'中药','止痛丸','安徽');
-insert into 药品 values (106,'草药','金银花','河南');
+-- 创建触发器，当药品被销售出去时，统计出库数据。
+go
+create trigger trig_update_chuku
+on 购买
+after insert
+as
+begin
+declare @yaopin int
+declare @num int
+declare @chuku int
+select @yaopin=药品编号,@num=数量 from inserted
+select @chuku=max(出库编号)+1 from 出库信息
+insert into 出库信息 values (@chuku,@yaopin,@num, getdate())
 
-insert into 销售员 values (1,'李冰冰','女','xxx1');
-insert into 销售员 values (2,'张伟','男','xfffxx1');
-insert into 销售员 values (3,'杨幂','女','11132号院');
-insert into 销售员 values (4,'徐聪','男','xxx2234');
-insert into 销售员 values (5,'孙逸凡','男','xxx343');
-insert into 销售员 values (6,'李一笑','女','xxx543');
-
-insert into 进货信息 values (101001,101,DEFAULT,'2020-01-07','2023-07-01',20,100);
-insert into 进货信息 values (101002,101,DEFAULT,'2020-03-07','2023-08-01',10,140);
-insert into 进货信息 values (102001,102,DEFAULT,'2020-01-07','2023-07-01',15,150);
-insert into 进货信息 values (103001,103,DEFAULT,'2020-01-07','2023-07-01',26,180);
-insert into 进货信息 values (104001,104,DEFAULT,'2020-01-07','2023-07-01',28,200);
-insert into 进货信息 values (105001,105,DEFAULT,'2020-01-07','2023-07-01',20,10);
-insert into 进货信息 values (106001,106,DEFAULT,'2020-01-07','2023-07-01',20,120);
-
-insert into 销售信息 values (101001,1,30,'G001',5,default);
-insert into 销售信息 values (101001,2,35,'G001',3,default);
-insert into 销售信息 values (101001,3,32,'G002',4,default);
-insert into 销售信息 values (102001,1,15,'G003',5,default);
-insert into 销售信息 values (103001,1,20,'G004',5,default);
+end
 
 
-/* 测试一：创建销售员与其所销售的药品的详细信息的视图 */
 
+/* 分析一：创建员工与其所销售的药品的数量和时间 */
+go
 create view xiaoshou_view
 as
-select t1.*, t2.柜台号,t2.进货编号,t2.数量,t2.销售单价,t2.销售日期
-from 销售员 t1, 销售信息 t2
-where t1.工号=t2.工号;
-
+select t1.*, t2.数量,t2.购买时间
+from 员工 t1, 购买 t2
+where t1.工号=t2.销售员;
+go
 select * from xiaoshou_view;
 
-/* 测试二：创建进货药品的详细信息视图 */
+/* 分析二：创建入库药品的详细信息视图 */
 
-create view jinhuo_view
-as
-select t1.*, t2.进货编号,t2.进货单价,t2.进货日期,t2.进货数量,t2.生产日期,t2.有效期
-from  药品 t1,进货信息 t2
-where t1.药品编号=t2.药品编号;
-
-select * from jinhuo_view;
-
--- 添加新的进货信息，即插入数据到进货表中
-
-use 中草药销售管理系统
 go
-create procedure insert_jh
-@jhnum int,
+create view ruku_view
+as
+select t1.*, t2.入库编号,t2.入库日期, t2.入库位置,t2.入库数量
+from  药品 t1
+join 入库信息 t2
+on t1.药品编号=t2.药品编号;
+go
+select * from ruku_view;
+
+-- 分析三：添加新的进货信息，即插入数据到进货表中
+
+go
+use 药品存销信息管理系统
+go
+create procedure insert_rk
+@rknum int,
 @ypnum int,
-@scdate date,
-@yxdate date,
-@price decimal,
+@rkdate date,
+@location varchar(20),
 @num int
 as
 begin
-insert into 进货信息 values(@jhnum,@ypnum,default,@scdate,@yxdate,@price,@num);
+insert into 入库信息 values(@rknum,@ypnum,@rkdate,@location,@num);
 end
+
+-- 分析四：当药品入库时，实时更新药品的数量
+
+go
+select * from 药品 where 药品名称='六味地黄丸';
+go
+insert into 入库信息 values(107001,101,'2022-06-17','位置1',20);
+go
+select * from 药品 where 药品名称='六味地黄丸';
+go
+
+-- 分析五：当药品被销售出去时，统计出库数据
+go
+select * from 出库信息 where 药品编号=101;
+go
+insert into 购买 values (11,201,101,getdate(),2,2);
+go
+select * from 出库信息 where 药品编号=101;
+
+-- 分析六：分组统计每个客户的购买的总药品数并计算购买总价
+
+select 客户.客户编号,客户.姓名, sum(购买.数量) 总数, sum(购买.数量*药品.价格) as 总价
+from 客户, 购买, 药品
+where 客户.客户编号=购买.客户编号
+and 药品.药品编号=购买.药品编号
+group by 客户.客户编号,客户.姓名
+
+
+-- 分析七：分组查询每个员工销售的每种药品数量，并按照销售量由大到小排序
+
+select 员工.工号,员工.姓名, sum(购买.数量) 总数
+from 员工, 购买, 药品
+where 员工.工号=购买.销售员
+and 药品.药品编号=购买.药品编号
+group by 员工.工号,员工.姓名
+order by 总数 desc
+
+-- 分析八：统计每种药品在2022年六月的出库量，包含所有药品信息
+
+select 药品.药品编号,药品.药品名称, sum(出库信息.数量) as 总出库量
+from 药品
+left join 出库信息 on 药品.药品编号=出库信息.药品编号
+where year(出库日期)=2022 and MONTH(出库日期)=6
+group by 药品.药品编号,药品.药品名称
